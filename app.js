c10eebb3ac912d49ff6b934f3c45980e40a03dc4
cayvowcorir <cayvowcorir@gmail.com>
2016-02-10 01:56:14 +0300

    Added amazon web services

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, function() {
+  console.log('App listening on port 3000');
+});
+app.use("/public",express.static('/public'));
+
+app.use('/', express.static(__dirname + '/'));
+
+
